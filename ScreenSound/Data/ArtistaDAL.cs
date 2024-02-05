e422145483180eaac7cccb05ce56458ca87fde11
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-02-05 18:05:04 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Data/ArtistaDAL.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.SqlClient;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Data;
+
+internal class ArtistaDAL
+{
+
+    public IEnumerable<Artista> ObterTodosOsArtistas()
+    {
+        var list = new List<Artista>();
+
+        using var connection = new Connection().ObterConeccao();
+        connection.Open();
+
+        string sql = "SELECT * FROM Artistas";
+        SqlCommand command = new SqlCommand(sql, connection);
+        using SqlDataReader reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            int idArtista = Convert.ToInt32(reader["Id"]);
+            string nomeArtista = Convert.ToString(reader["Nome"]);
+            string bioArtista = reader["Bio"].ToString();
+            Artista artista = new(nomeArtista, bioArtista)
+            {
+                Id = idArtista
+            };
+
+            list.Add(artista);
+        }
+
+        return list;
+    }
+
+    public void AdicionarArtista(Artista artista)
+    {
+        using var connection = new Connection().ObterConeccao();
+        connection.Open();
+
+        string sql = "INSERT INTO Artistas (Nome, FotoPerfil, Bio) VALUES (@Nome, @FotoPerfil, @Bio)";
+        SqlCommand command = new SqlCommand(sql, connection);
+        command.Parameters.AddWithValue("@Nome", artista.Nome);
+        command.Parameters.AddWithValue("@FotoPerfil", artista.FotoPerfil);
+        command.Parameters.AddWithValue("@Bio", artista.Bio);
+        var res = command.ExecuteNonQuery();
+
+        Console.WriteLine($"Linhas afetadas {res}" );
+    }
+}
